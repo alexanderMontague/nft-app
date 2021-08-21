58c0b6aba0593b7d5e3307c47326e97ad0c8af58
alexanderMontague <xturnermonty@gmail.com>
2021-08-20 23:16:22 -0400

    typescript and yarn workspaces

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,4 +1,6 @@
-import { task } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -11,12 +13,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+export default {
   solidity: "0.8.4",
-};
+} as HardhatUserConfig;
--- packages/hardhat/scripts/deploy.ts
+++ packages/hardhat/scripts/deploy.ts
@@ -1,18 +1,6 @@
 import { ethers } from "hardhat";
 
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-// const hre = require("hardhat");
-
 async function main() {
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
   const [deployer] = await ethers.getSigners();
   const balance = await deployer.getBalance();
   console.log(`Deploying contracts with the address: ${deployer.address}`);
@@ -32,8 +20,6 @@ async function main() {
   console.log("Token deployed to:", token.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch(error => {
